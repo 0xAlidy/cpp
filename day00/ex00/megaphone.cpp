b26cdc741cb096d0d7e17f2b691f04dd644f3374
codetox <axel_lidy@outlook.fr>
2021-05-25 16:17:30 +0200

    patch ex00

--- day00/ex00/megaphone.cpp
+++ day00/ex00/megaphone.cpp
@@ -6,19 +6,12 @@
 /*   By: alidy <alidy@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/18 16:01:48 by alidy             #+#    #+#             */
-/*   Updated: 2021/05/25 15:37:27 by alidy            ###   ########lyon.fr   */
+/*   Updated: 2021/05/25 16:01:36 by alidy            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
-char    to_uppercase(char c)
-{
-    if (c >= 'a' && c <= 'z')
-        return (c - 32);
-    else
-        return (c);
-}
+#include <cctype>
 
 int     main(int argc, char const *argv[])
 {
@@ -29,13 +22,14 @@ int     main(int argc, char const *argv[])
     {
         while (argv[i][y])
         {
-            std::cout << to_uppercase(argv[i][y]);
+            std::cout << (char)toupper(argv[i][y]);
             y++;
         }
         y = 0;
         i++;
     }
     if (argc == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    std::cout << std::endl;
     return (0);
 }
